0c85aeda1bfd098be6348fc0cf4181e808349fda
Alejandro <adgm1988@gmail.com>
2019-07-25 01:07:51 -0500

    2019-25-07 noche

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -3,16 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Carbon\Carbon;
 
 class CartController extends Controller
 {
     //
     public function update(){
-    	$cart=auth()->user()->cart;
+    	$client = auth()->user();
+    	$cart = $client->cart;
     	$cart->status = 'Pending';
+    	$cart->order_date = Carbon::now();
     	$cart->save();
 
-    	$notification='Tu pedido se ha registrado correctamente, te contactaremos pronto via email';
+    	$admins = User:where('admin',true)->get(); //usuarios admins
+    	Mail:to($admins)->send(new NewOrder($client, $cart)); //
+
+    	$notification = 'Tu pedido se ha registrado correctamente, te contactaremos pronto via email';
     	return back()->with(compact('notification'));
     }
 }
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -28,7 +28,7 @@
     <div class="container">
         <div class="row">
             <div class="col-md-6">
-                <h1 class="title">Write the best title for your page.</h1>
+                <h1 class="title">Bienvenido a {{ config('app.name') }}</h1>
                 <h4>Every landing page needs a small description after the big bold title, that's why we added this text here. Add here all the information that can make you or your product create the first impression.</h4>
                 <br />
                 <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="btn btn-danger btn-raised btn-lg">
